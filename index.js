45d7e8aac05cc76a99d7f301539e03dd31acc683
ruchibajra <ruchibajracharya8@gmail.com>
2024-07-28 23:16:32 +0545

    tictactoe css

---
+++ index.js
@@ -0,0 +1,47 @@
+// Practice Questions
+
+// Qs1. Get user to input a number using prompt("Enter a number:"). Check if the number is a multiple of 5 or not.
+
+// let num = prompt("Enter a number");
+
+// if(num%5 === 0){
+//     alert('It IS a multiple of 5');
+// }else{
+//     alert('It is NOT a multiple of 5');
+// }
+
+
+/*
+Qs2. Write a code which can give grades to students according to their scores
+90-100, A
+80-89 B
+70-79, C
+60-69, D
+50-59, E
+0-49, F 
+*/
+
+// let score = 100;
+// let grade;
+
+// if(score >= 90 && score <=100){
+//     grade = 'A';
+//     console.log("Grade: " + grade);
+// } else if (score >= 80 && score <=89){
+//     grade = 'B';
+//     console.log("Grade: " + grade);
+// }else if (score >= 70 && score <=79){
+//     grade = 'C';
+//     console.log("Grade: " + grade);
+// }else if(score >= 60 && score <=69){
+//     grade = 'D';
+//     console.log("Grade: " + grade);
+// } else if(score >= 50 && score <=59){
+//     grade = 'E';
+//     console.log("Grade: " + grade);
+// } else if (score >= 0 && score <=49){
+//     grade = 'F';
+//     console.log("Grade: " + grade);
+// } else{
+//     console.log("Invalid Data");
+// }
